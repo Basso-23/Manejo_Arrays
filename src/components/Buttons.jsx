4ad7c8d7f37f055dfe3f3c2047ea4b3d87c53b77
Carlos Baso <110511071+Basso-23@users.noreply.github.com>
2024-02-29 23:02:45 -0500

    modificando cosasitas

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,9 +1,9 @@
-const Buttons = ({ name, action, modifier }) => {
+const Buttons = ({ name, action, data }) => {
   return (
     <div
       onClick={() => {
         {
-          action(modifier);
+          action(data);
         }
       }}
       className=" bg-sky-600 w-fit px-6 py-2 cursor-pointer mt-20 text-white"
--- src/pages/Incio.jsx
+++ src/pages/Incio.jsx
@@ -18,15 +18,19 @@ const Inicio = () => {
   };
 
   const addItem = (toAdd) => {
+    //Añade el objeto al JSON que recibe de "toAdd"
     setProducts((prevState) => [...prevState, toAdd]);
   };
 
   const deleteItem = (toDelete) => {
+    //Filtra el JSON dejando todos los objetos excepto el que objeto que dentro de "item.id" posee el mismo valor que recibe de "toDelete"
     const newItems = products.filter((item) => item.id !== toDelete);
+    //Actualiza el JSON
     setProducts(newItems);
   };
 
   const checkItem = (toFind) => {
+    //Busca en "item.id" si el valor que recibe de "toFind" existe en el JSON
     if (products.find((item) => item.id == toFind)) {
       console.log("existe");
     } else {
@@ -34,13 +38,39 @@ const Inicio = () => {
     }
   };
 
-  const updateItem = (toUpdate) => {
-    if (products.find((item) => item.id == 3)) {
+  const plusQty = (toFind) => {
+    //Busca en "item.id" si el valor que recibe de "toFind" existe en el JSON
+    if (products.find((item) => item.id == toFind)) {
       const updatedItems = products;
-      const objIndex = updatedItems.findIndex((item) => item.id == 3);
+      //Encuentra el index del valor a cambiar
+      const objIndex = products.findIndex((item) => item.id == toFind);
+      //Aqui se modifica el valor
       updatedItems[objIndex].cant += 1;
+      //Actualiza el JSON
       setProducts(updatedItems);
+      //Rerender al JSON
       setRender(!render);
+    } else {
+      console.log("NO existe");
+    }
+  };
+  const minusQty = (toFind) => {
+    //Busca en "item.id" si el valor que recibe de "toFind" existe en el JSON
+    if (products.find((item) => item.id == toFind)) {
+      const updatedItems = products;
+      //Encuentra el index del valor a cambiar
+      const objIndex = products.findIndex((item) => item.id == toFind);
+      //Controla que la cantidad no pueda ser negativa
+      if (updatedItems[objIndex].cant !== 1) {
+        //Aqui se modifica el valor
+        updatedItems[objIndex].cant -= 1;
+        //Actualiza el JSON
+        setProducts(updatedItems);
+        //Rerender al JSON
+        setRender(!render);
+      } else {
+        console.log("La cantidad es CERO");
+      }
     } else {
       console.log("NO existe");
     }
@@ -60,14 +90,11 @@ const Inicio = () => {
         ))}
       </div>
       <div className=" flex gap-10">
-        <Buttons name={"Add"} action={addItem} modifier={book} />
-        <Buttons
-          name={"Delete"}
-          action={deleteItem}
-          modifier={products.length}
-        />
-        <Buttons name={"Check"} action={checkItem} modifier={3} />
-        <Buttons name={"Update"} action={updateItem} modifier={"Jane Doe"} />
+        <Buttons name={"Add"} action={addItem} data={book} />
+        <Buttons name={"Delete"} action={deleteItem} data={products.length} />
+        <Buttons name={"Check"} action={checkItem} data={2} />
+        <Buttons name={"+"} action={plusQty} data={2} />
+        <Buttons name={"-"} action={minusQty} data={2} />
       </div>
     </div>
   );
